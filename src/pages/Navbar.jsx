906e450f90d07483717985c0dcfc98233b271d5c
mitali1411 <mittali.rasaniya1411@gmail.com>
2025-11-13 18:30:06 +0530

    project added

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -13,16 +13,16 @@ const Navbar = () => {
     <a href="#contact">Contact Me</a>
   </nav>
 
-  <div class="dropdown">
-  <button class="btn btn-info" id='nav-list' type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    <i class='bx bx-menu' ></i>
+  <div className="dropdown">
+  <button className="btn btn-info" id='nav-list' type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    <i className='bx bx-menu' ></i>
   </button>
-  <ul class="dropdown-menu">
-    <li><button class="dropdown-item" type="button"><a href="#" className='active'>Home</a></button></li>
-    <li><button class="dropdown-item" type="button"><a href="#about">About</a></button></li>
-    <li><button class="dropdown-item" type="button"><a href="#skills">Skills</a></button></li>
-    <li><button class="dropdown-item" type="button"><a href="#projects" >Projects</a></button></li>
-    <li><button class="dropdown-item" type="button"><a href="#contact">Contact Me</a></button></li>
+  <ul className="dropdown-menu">
+    <li><button className="dropdown-item" type="button"><a href="#" className='active'>Home</a></button></li>
+    <li><button className="dropdown-item" type="button"><a href="#about">About</a></button></li>
+    <li><button className="dropdown-item" type="button"><a href="#skills">Skills</a></button></li>
+    <li><button className="dropdown-item" type="button"><a href="#projects" >Projects</a></button></li>
+    <li><button className="dropdown-item" type="button"><a href="#contact">Contact Me</a></button></li>
   </ul>
 </div>
 </header>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -8,94 +8,37 @@ import project6 from '../assets/QR-Generator.png'
 import project7 from '../assets/BMI.png'
 import project8 from '../assets/AuthApp.png'
 import project9 from '../assets/project9.png'
-
+import project10 from '../assets/11.png'
+import project11 from '../assets/12.png'
 
 const Projects = () => {
+  const project = [
+    {Image: project11, Title: "INS Theatre", Description: 'Developed a full-stack theatre web app with a dynamic frontend and admin panel for content management.', Link: 'https://www.indravatinatyasamiti.com/'},
+    {Image: project10, Title: "Sehat Ki Pathshala", Description: "Health website build on React and Designed with bootstrap", Link: 'https://sehat-ki-pathshala-ewcm.vercel.app/'},
+    {Image: project1, Title: 'Cryptomania', Description: 'Build on Redux toolkit, MaterialUI for UI, integrated trending crypto API using axios.', Link: 'https://cryptomania-iota.vercel.app/'},
+    {Image: project8, Title: 'Authentication App', Description: 'Build on Redux Toolkit, here user can login and register, without doing so, he will unable to reach Home Page.', Link: 'https://authentication-app-ashen-delta.vercel.app/register'},
+    {Image: project2, Title: 'News', Description: 'Build on React.js, using Bootstrap for UI design, intergrated news api using axios.', Link: 'https://news-app-six-sable.vercel.app/'},
+    {Image: project3, Title: 'KhataBook', Description: 'Build on React.js, using Bootstrap for UI design, applying CRUD functionality.', Link: 'https://khatabook-hazel.vercel.app/'},
+    {Image: project4, Title: 'Weather App', Description: 'Build using JavaScript, integrated Weather API using fetch method to fetch anywhere weather data.', Link: 'https://weather-app-psi-sable.vercel.app/'},
+    {Image: project5, Title: 'JS Todo', Description: 'Build on JavaScript, here we can save todos and delete todos.', Link: 'https://js-todos.vercel.app/'},
+    {Image: project6, Title: 'QR Generator', Description: 'Build on JavaScript, intergrated QR api using fetch method to generate QR for entered URL or text.', Link: 'https://qr-generator-one-gamma.vercel.app/'},
+    {Image: project7, Title: 'BMI Calculator', Description: 'Build on JavaScript, here we can calculated our BMI that shows us that we are either healthy, overweight, undeweight or obese.', Link: 'https://bmi-calculator-seven-psi.vercel.app/'},
+    {Image: project9, Title: 'My Portfolio', Description: 'My personal portfolio build on React, HTML & CSS'}
+  ]
   return (
     <section id='projects' className="project-section">
       <h3 className='mt-4'>MY <span>PROJECTS</span></h3>
-
       <div className="projects">
-      <div className="box1">
-        <img src={project1} alt="" />
-        <div className="layer">
-          <h4>Cryptomania</h4>
-          <p>Build on Redux toolkit, MaterialUI for UI, integrated trending crypto API using axios.</p>
-          <a href="https://cryptomania-iota.vercel.app/" target='_blank'><i class='bx bx-link-external'></i></a>
-        </div>
-         </div>
-
-      <div className="box1">
-        <img src={project8} alt="" />
-        <div className="layer">
-          <h4>Authentication App</h4>
-          <p>Build on Redux Toolkit, here user can login and register, without doing so, he'll unable to reach Home Page.</p>
-          <a href="https://authentication-app-ashen-delta.vercel.app/register" target='_blank'><i class='bx bx-link-external'></i></a>
-        </div>
-      </div>
-
-      <div className="box1">
-        <img src={project2} alt="" />
-        <div className="layer">
-          <h4>News</h4>
-          <p>Build on React.js, using Bootstrap for UI design, intergrated news api using axios.</p>
-          <a href="https://news-app-six-sable.vercel.app/" target='_blank'><i class='bx bx-link-external'></i></a>
-        </div>
-      </div>
-
-      <div className="box1">
-        <img src={project3} alt="" />
-        <div className="layer">
-          <h4>KhataBook</h4>
-          <p>Build on React.js, using Bootstrap for UI design, applying CRUD functionality. </p>
-          <a href="https://khatabook-hazel.vercel.app/" target='_blank'><i class='bx bx-link-external'></i></a>
-        </div>
-      </div>
-
-      <div className="box1">
-        <img src={project4} alt="" />
-        <div className="layer">
-          <h4>Weather API</h4>
-          <p>Build using JavaScript, integrated Weather API using fetch method to fetch anywhere weather data.</p>
-          <a href="https://weather-app-psi-sable.vercel.app/" target='_blank'><i class='bx bx-link-external'></i></a>
-        </div>
-      </div>
-
-      <div className="box1">
-        <img src={project5} alt="" />
-        <div className="layer">
-          <h4>JS Todo</h4>
-          <p>Build on JavaScript, here we can save todos and delete todos. </p>
-          <a href="https://js-todos.vercel.app/" target='_blank'><i class='bx bx-link-external'></i></a>
-        </div>
-      </div>
-
-      <div className="box1">
-        <img src={project6} alt="" />
-        <div className="layer">
-          <h4>QR Generator</h4>
-          <p>Build on JavaScript, intergrated QR api using fetch method to generate QR for entered URL or text.</p>
-          <a href="https://qr-generator-one-gamma.vercel.app/" target='_blank'><i class='bx bx-link-external'></i></a>
-        </div>
-      </div>
-
-      <div className="box1">
-        <img src={project7} alt="" />
-        <div className="layer">
-          <h4>BMI Calculator</h4>
-          <p>Build on JavaScript, here we can calculated our BMI that shows us that we are either healthy, overweight, undeweight or obese.</p>
-          <a href="https://bmi-calculator-seven-psi.vercel.app/" target='_blank'><i class='bx bx-link-external'></i></a>
-        </div>
-      </div>
-
-      <div className="box1">
-        <img src={project9} alt="" />
-        <div className="layer">
-          <h4>My Portfolio</h4>
-          <p>Build on React, HTML & CSS</p>
-          <a href="https://my-portfolio-eta-rose-79.vercel.app/" target='#'><i class='bx bx-link-external'></i></a>
-        </div>
-      </div>
+      {project.map((item, index) => 
+          <div className="box1">
+            <img src={item.Image} alt="" />
+            <div className="layer">
+              <h4>{item.Title}</h4>
+              <p>{item.Description}</p>
+              <a href={item.Link} target='#'><i class='bx bx-link-external'></i></a>
+            </div>
+          </div>
+      )}
       </div>
     </section>
   )
